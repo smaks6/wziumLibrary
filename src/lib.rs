9225e318f9b5122198c9f757f1a0195103de2326
smaks6 <smaks2313@gmail.com>
2022-01-13 18:32:54 +0100

    1

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod tests {
+    use crate::wzium_lib::wzium;
+
+    #[test]
+    fn it_works() {
+        println!("{}", wzium(true));
+    }
+}
+
+pub mod wzium_lib{
+    pub fn wzium(print_certificate: bool)-> String{
+        if print_certificate{
+            println!("===============================");
+            println!("This is official wzium library!");
+            println!("===============================");
+        }
+        String::from("WZIUM")
+    }
+}
